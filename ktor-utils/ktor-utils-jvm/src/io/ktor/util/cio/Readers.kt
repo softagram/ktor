a2ed7ea369460e93aeb8c6af2bf87bc3e92e61a4
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-11-26 13:02:26 +0300

    Fix CIO client early selector cancellation

--- ktor-utils/ktor-utils-jvm/src/io/ktor/util/cio/Readers.kt
+++ ktor-utils/ktor-utils-jvm/src/io/ktor/util/cio/Readers.kt
@@ -8,14 +8,14 @@ import java.nio.*
 /**
  * Convert [ByteReadChannel] to [ByteArray]
  */
-suspend fun ByteReadChannel.toByteArray(limit: Int = Int.MAX_VALUE): ByteArray =
+suspend inline fun ByteReadChannel.toByteArray(limit: Int = Int.MAX_VALUE): ByteArray =
     readRemaining(limit.toLong()).readBytes()
 
 /**
  * Read data chunks from [ByteReadChannel] using buffer
  */
 @InternalAPI
-suspend fun ByteReadChannel.pass(buffer: ByteBuffer, block: suspend (ByteBuffer) -> Unit) {
+suspend inline fun ByteReadChannel.pass(buffer: ByteBuffer, block: (ByteBuffer) -> Unit) {
     while (!isClosedForRead) {
         buffer.clear()
         readAvailable(buffer)
@@ -28,7 +28,7 @@ suspend fun ByteReadChannel.pass(buffer: ByteBuffer, block: suspend (ByteBuffer)
 /**
  * Executes [block] on [ByteWriteChannel] and close it down correctly whether an exception
  */
-suspend fun ByteWriteChannel.use(block: suspend ByteWriteChannel.() -> Unit) {
+inline fun ByteWriteChannel.use(block: ByteWriteChannel.() -> Unit) {
     try {
         block()
     } catch (cause: Throwable) {
