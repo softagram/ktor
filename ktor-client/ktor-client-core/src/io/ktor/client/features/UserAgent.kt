74f500f9a581537ea0c211447f873d19d59f235b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-11-26 17:12:49 +0300

    Add default user agent feature

---
+++ ktor-client/ktor-client-core/src/io/ktor/client/features/UserAgent.kt
@@ -0,0 +1,36 @@
+package io.ktor.client.features
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.util.*
+
+class UserAgent(val agent: String) {
+
+    class Config(var agent: String = "Ktor http-client")
+
+    companion object Feature : HttpClientFeature<Config, UserAgent>{
+        override val key: AttributeKey<UserAgent> = AttributeKey("UserAgent")
+
+        override fun prepare(block: Config.() -> Unit): UserAgent = UserAgent(Config().apply(block).agent)
+
+        override fun install(feature: UserAgent, scope: HttpClient) {
+            scope.requestPipeline.intercept(HttpRequestPipeline.State) {
+                context.header(HttpHeaders.UserAgent, feature.agent)
+            }
+        }
+    }
+}
+
+fun HttpClientConfig<*>.BrowserUserAgent() {
+    install(UserAgent) {
+        agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/70.0.3538.77 Chrome/70.0.3538.77 Safari/537.36"
+    }
+}
+
+fun HttpClientConfig<*>.CurlUserAgent() {
+    install(UserAgent) {
+        agent = "curl/7.61.0"
+    }
+}
+
